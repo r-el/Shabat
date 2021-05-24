7c5b3d6cb666eb959ddc72420674c612cd75e26d
rel <rel@mail.com>
2021-05-24 15:47:49 +0300

    Add ParsePhoto Class

---
+++ Models/ParsePhoto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shabat2.Models
+{
+
+    // המרת תמונה
+    public class ParsePhoto
+    {
+        public byte[] Get(IFormFile file)
+        {
+
+            // אם לא קיבלת כלום תחזיר null
+            if (file == null) return null;
+
+            // סטרים שומר איזשהו קובץ בזיכרון 
+            MemoryStream stream = new MemoryStream();
+            file.CopyTo(stream);
+
+            // תמיר אותו למערך בייטים
+            return stream.ToArray();
+        }
+    }
+}
