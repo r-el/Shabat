eca080cc616dd29d098f20ef65c0c23ce1198e7f
rel <rel@mail.com>
2021-05-24 15:58:25 +0300

    Add Food Class (FoodNmae, Photo, SetPhoto function)

---
+++ Models/Food.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shabat2.Models
+{
+    public class Food
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "שם מאכל")]
+        public string FoodName { get; set; }
+
+        [Display(Name = "תמונה")]
+        public byte[] Photo { get; set; }
+
+
+        // המרת התמונה לבייטים
+        public IFormFile SetPhoto { set { Photo = new ParsePhoto().Get(value); } }
+    }
+}
