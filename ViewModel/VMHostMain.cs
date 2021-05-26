11758511b33bab5b445a12bb253289ef3dcbeade
rel <rel@mail.com>
2021-05-26 19:39:18 +0300

    Add VNHostMain

---
+++ ViewModel/VMHostMain.cs
@@ -0,0 +1,15 @@
+﻿using shabat2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shabat2.ViewModel
+{
+    public class VMHostMain
+    {
+        public VMHostMain(){ }
+        public List<Category> Categories { get; set; }
+        public List<Guest> Guests { get; set; }
+    }
+}
