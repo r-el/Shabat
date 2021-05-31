26a4e7aa9eee92e9b06c3b34fca6d652bd09b2fc
rel <rel@mail.com>
2021-05-31 16:27:00 +0300

    Add VMAddCategory

---
+++ ViewModel/VMAddCategory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using shabat2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shabat2.ViewModel
+{
+    public class VMAddCategory
+    {
+        public VMAddCategory() { }
+
+        public Category Category { get; set; }
+
+        [Display(Name = "הוסף תמונה")]
+        public IFormFile File { get; set; }
+
+        [Display(Name = "שם מאכל")]
+        public string Food { get; set; }
+
+        public IFormFile FoodFile { get; set; }
+    }
+}
