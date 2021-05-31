e36b7f68033085abe776526dc69ca7dda57df108
rel <rel@mail.com>
2021-05-31 15:46:53 +0300

    Add VMAddFood

---
+++ ViewModel/VMAddFood.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using shabat2.Models;
+
+namespace shabat2.ViewModel
+{
+    public class VMAddFood
+    {
+        public VMAddFood() { Categories = new List<Category>(); }
+
+        public Category Category { get; set; }          // קבוצה
+        public int CategoryId { get; set; }             // מזהה קטגוריה
+        public List<Category> Categories { get; set; }  // רשימת קבוצות
+        public Food Food { get; set; }                  // מאכל
+        [Display(Name = "הוספת תמונה")]
+        public IFormFile File { get; set; }             // תמונה
+    }
+}
