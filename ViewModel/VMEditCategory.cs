53c624d3b76849d8bb1078271dd1cc87896f6061
rel <rel@mail.com>
2021-06-02 17:09:52 +0300

    Add VMAddCategory

---
+++ ViewModel/VMEditCategory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using shabat2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shabat2.ViewModel
+{
+    public class VMEditCategory
+    {
+        public VMEditCategory(){}
+        public Category Category { get; set; }      // קבוצה
+        public int CategoryID { get; set; }         // מזהה קטגוריה
+        [Display(Name="החלף תמונה")]
+        public IFormFile File { get; set; }         // תמונה
+    }
+}
